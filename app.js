bac656ee6e29b0dd930de83ed3c8833c7a48a2d5
Alexis hnr <halexis.92@gmail.com>
2024-11-17 01:47:51 +0100

    v1

--- app.js
+++ app.js
@@ -53,7 +53,7 @@ app.use(session({
   resave: false,
   saveUninitialized: true,
   proxy: true, // Required for Heroku & Digital Ocean (regarding X-Forwarded-For)
-  name: 'MyCoolWebAppCookieName', // This needs to be unique per-host.
+  name: 'MoanaEnergy', // This needs to be unique per-host.
   cookie: {
     secure: true, // required for cookies to work on HTTPS
     httpOnly: false,
